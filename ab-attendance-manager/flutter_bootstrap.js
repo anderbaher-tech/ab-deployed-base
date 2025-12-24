449cf39478ae875120a05fcfefff7e9229a1d5d8
anderbaher-tech <tech@anderbaher.com>
2025-12-24 18:55:10 +0530

    ABTCT-381

--- ab-attendance-manager/flutter_bootstrap.js
+++ ab-attendance-manager/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2847214023"
+    serviceWorkerVersion: "3879368872"
   }
 });
